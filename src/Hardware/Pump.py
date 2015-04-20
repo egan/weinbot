7a776c85e22e2781ea76a97ae9fa545011955772
natrucha <natapueyo@gmail.com>
2015-04-20 13:23:34 -0700

    Created a Pump class for control through GPIO.

---
+++ src/Hardware/Pump.py
@@ -0,0 +1,46 @@
+##
+# Pump.py: Class implementing control of WEINBot water pump via GPIO.
+#
+# Copyright 2015, Natalie Pueyo Svoboda
+#
+##
+
+import Adafruit_BBIO.GPIO as GPIO
+import logging
+
+class Pump():
+    """
+        Pump: A class that provides an interface to control the WEINBot
+                 water pump via GPIO.
+    """
+
+    def __init__(self, pin="P8_XX"):
+        """
+            pin: BBB GPIO pin to control the water pump.
+
+        """
+
+        # Set up GPIO.
+        self.pin = pin
+        GPIO.setup(pin, GPIO.OUT)
+        return None
+
+    def __del__(self):
+        self.stop()
+        return
+
+    def start(self):
+        """
+            start: Start the pump.
+
+        """
+        logging.debug("pump: started")
+        GPIO.output(self.pin, GPIO.HIGH)
+
+    def stop(self):
+        """
+            stop: Stop the pump.
+
+        """
+        logging.debug("pump: stopped")
+        GPIO.output(self.pin, GPIO.LOW)
