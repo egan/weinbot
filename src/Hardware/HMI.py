9fef92e3d8c705c6316c5ff9239a5f75c142052f
Egan McComb <egan.mccomb@gmail.com>
2015-04-26 20:18:12 -0700

    Slight refactoring.

--- src/Hardware/HMI.py
+++ src/Hardware/HMI.py
@@ -1,5 +1,5 @@
 ##
-# Hardware.py: Class implementing control of WEINBot Human Machine Interface.
+# HMI.py: Class implementing reading of WEINBot Human Machine Interface.
 #
 # Copyright 2015, Natalie Pueyo Svoboda
 #
@@ -10,19 +10,19 @@ import logging
 
 class HMI():
     """
-        HMI: A class that provides an interface to read the HMI switches to 
-               select the desired WEINBot demo.
+        HMI: A class that provides an interface to read the HMI switches to
+             select the desired WEINBot demo.
     """
 
-    def __init__(self, pins_selector=("P8_37", "P8_38", "P8_39", "P8_40", "P8_41", "P8_42", "P8_43", "P8_44")):
+    def __init__(self, pins=("P8_37", "P8_38", "P8_39", "P8_40", "P8_41", "P8_42", "P8_43", "P8_44")):
         """
-            pins_selector: BBB GPIO pins to read state of HMI switch array.
+            pins: BBB GPIO pins to read state of HMI switch array.
 
         """
 
         # Set up GPIO.
-        self.pins_selector = pins_selector
-        for pin in pins_selector:
+        self.pins = pins
+        for pin in pins:
             GPIO.setup(pin, GPIO.IN)
 
         return None
@@ -31,35 +31,36 @@ class HMI():
         self.stop()
         return
 
-    def HMI_state(self):
+    def read(self):
         """
-            HMI_state: Read GPIO pins to determine what demo will be set.
+            read: Read GPIO pins to determine what demo will be set.
+
         """
         state = 0
 
         # Read GPIO pins to determine the state of each switch
-        if GPIO.input(self.pins_selector[0]):
+        if GPIO.input(self.pins[0]):
             state = state + 1
 
-        if GPIO.input(self.pins_selector[1]):
+        if GPIO.input(self.pins[1]):
             state = state + 2
 
-        if GPIO.input(self.pins_selector[2]):
+        if GPIO.input(self.pins[2]):
             state = state + 4
 
-        if GPIO.input(self.pins_selector[3]):
+        if GPIO.input(self.pins[3]):
             state = state + 8
 
-        if GPIO.input(self.pins_selector[4]):
+        if GPIO.input(self.pins[4]):
             state = state + 16
 
-        if GPIO.input(self.pins_selector[5]):
+        if GPIO.input(self.pins[5]):
             state = state + 32
 
-        if GPIO.input(self.pins_selector[6]):
+        if GPIO.input(self.pins[6]):
             state = state + 64
 
-        if GPIO.input(self.pins_selector[7]):
+        if GPIO.input(self.pins[7]):
             state = state + 128
 
         return state
