bada427df81b16cb12baf46a5c534303c770cc47
Egan McComb <egan.mccomb@gmail.com>
2015-05-23 16:55:43 -0700

    Rework shutdown to use BOTH trigger and check HI/LO.

--- src/Hardware/Shutoff.py
+++ src/Hardware/Shutoff.py
@@ -11,30 +11,35 @@ import logging
 
 class Shutoff():
     """
-        Shutoff: A class implementing shutdown procedures upon receiving
-                 shutdown interrupt on GPIO from NO pole of emergency shutoff
-                 switch.
+        Shutoff: A class implementing shutdown procedures upon receiving rising
+                 edge interrupt on GPIO from NO pole of emergency shutoff
+                 switch or sensing edges.
     """
 
-    def __init__(self, pin, gpio_mode=(GPIO.RISING, GPIO.FALLING), objects=[]):
+    def __init__(self, pin, objects=[]):
         """
             pin:       GPIO pin on which to wait for rising edge.
-            gpio_mode: type of edge to trigger, reset
             objects:   list of objects to call stop() methods for.
 
         """
         self.pin = pin
-        self.gpio_mode = gpio_mode
         self.objects = objects
 
         # Set up GPIO interrupt on rising edge.
         GPIO.setup(pin, GPIO.IN)
-        GPIO.add_event_detect(pin, gpio_mode[0], callback=self.shutdown, bouncetime=1000)
+        GPIO.add_event_detect(pin, GPIO.BOTH, callback=self.__handler, bouncetime=500)
         return None
 
     def __del__(self):
         self.shutdown()
         return
+
+    def __handler(self):
+        if GPIO.input(self.pin):
+            return
+        else:
+            self.shutdown(1)
+            logging.debug("shutoff: waiting for reset")
 
     def shutdown(self, channel):
         """
@@ -44,12 +49,8 @@ class Shutoff():
 
         """
         # Shut down Hardware objects.
-        logging.debug("shutoff: shutdown interrupt received")
+        logging.debug("shutoff: shutdown event received")
         for obj in self.objects:
             obj.stop()
-        # XXX: Other shutdown tasks.
 
-        # Wait until switch is reset.
-        logging.debug("shutoff: waiting for reset")
-        GPIO.wait_for_edge(self.pin, self.gpio_mode[1])
         return
