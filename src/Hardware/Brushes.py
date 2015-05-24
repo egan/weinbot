16fe7c6fb64466542de2e73baa50b289d4d71886
Egan McComb <egan.mccomb@gmail.com>
2015-05-23 18:20:01 -0700

    Initial commit of brush motor ramp....

--- src/Hardware/Brushes.py
+++ src/Hardware/Brushes.py
@@ -6,24 +6,41 @@
 #
 ##
 
+from __future__ import division
 import Adafruit_BBIO.GPIO as GPIO
 import logging
+import serial
+import threading
+import time
 
 class Brushes():
     """
         Brushes: A class that provides an interface to control the WEINBot
-                 brush motor relay via GPIO.
+                 brush motor relay via GPIO and simple serial.
     """
 
-    def __init__(self, pin="P8_11"):
+    def __init__(self, pin="P8_11", port="/dev/ttyO1", rampTime=2):
         """
-            pin: BBB GPIO pin to control brush motor relay.
+            pin:      BBB GPIO pin to control brush motor relay.
+            port:     Teletypewriter device to connect to.
+            rampTime: Time in to ramp (s).
 
         """
 
         # Set up GPIO.
         self.pin = pin
         GPIO.setup(pin, GPIO.OUT)
+
+        # Set up serial.
+        self.saber = serial.Serial()
+        self.saber.baudrate = 9600
+        self.saber.port = '/dev/%s' % (self.port)
+        self.saber.open()
+        self.isOpen = self.saber.isOpen()
+
+        # Parameters
+        self.rampTime = rampTime
+        self.steps = 4
         return None
 
     def __del__(self):
@@ -37,6 +54,24 @@ class Brushes():
         """
         logging.debug("brushes: started")
         GPIO.output(self.pin, GPIO.HIGH)
+        t = thread.Thread(target=self.__ramp)
+        t.start()
+        return
+
+    def __ramp(self):
+        tstep = self.rampTime/3
+        logging.debug("brushes: 25%")
+        self.saber.write(95)
+        time.sleep(tstep)
+        logging.debug("brushes: 50%")
+        self.saber.write(63)
+        time.sleep(tstep)
+        logging.debug("brushes: 75%")
+        self.saber.write(31)
+        time.sleep(tstep)
+        logging.debug("brushes: 100%")
+        self.saber.write(0)
+        return
 
     def stop(self):
         """
@@ -44,4 +79,5 @@ class Brushes():
 
         """
         logging.debug("brushes: stopped")
+        self.saber.write(127)
         GPIO.output(self.pin, GPIO.LOW)
